bbd919aa611958d0d7985c20a458618174b5cb7e
nickolay.grin <grinmail84@gmail.com>
2022-05-13 11:48:53 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -139,6 +139,26 @@ $(document).ready(function() {
 		]
 	});
 
+	   var show = true;
+    var countbox = ".item-number__value";
+    $(window).on("scroll load resize", function () {
+        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+        var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+        var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+        var w_height = $(window).height(); // Высота окна браузера
+        var d_height = $(document).height(); // Высота всего документа
+        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+            $('.item-number__value span').css('opacity', '1');
+            $('.item-number__value span').spincrement({
+                thousandSeparator: "",
+                duration: 2000
+            });
+             
+            show = false;
+        }
+    });
+ 
 	$('.phone-block__link').click(function(event) {
 		event.preventDefault();
 		$(this).parent().addClass("active");
